e4831bae699c4fb75bddf9e5ac387eb2d60d6d91
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-03-12 15:38:03 +0000

    margin

--- app/page.js
+++ app/page.js
@@ -3,6 +3,11 @@ export default function Page(){
   return (
     <iframe
       src="/index.html"
+      style={{
+        width:"100%",
+        height:"100vh",
+        border:"none"
+      }}
     />
   )
 
