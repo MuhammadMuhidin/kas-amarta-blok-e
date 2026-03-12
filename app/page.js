cf8592d73dd852076f60a4433defb5c3ba0b4735
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-03-12 15:56:41 +0000

    fixed lyout

--- app/page.js
+++ app/page.js
@@ -4,9 +4,11 @@ export default function Page(){
     <iframe
       src="/index.html"
       style={{
-        width:"100%",
-        height:"100vh",
-        border:"none"
+        position: "fixed",
+        inset: 0,
+        width: "100vw",
+        height: "100vh",
+        border: "none"
       }}
     />
   )
