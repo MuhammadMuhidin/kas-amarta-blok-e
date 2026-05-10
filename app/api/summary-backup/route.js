7fc76305d0ed82b7bdc3ed368786047576c95041
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-10 10:10:32 +0000

    edit rcp naming

--- app/api/summary-backup/route.js
+++ app/api/summary-backup/route.js
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabase"
 export async function GET(){
 
   const { data,error } = await supabase
-    .rpc("cashflow_summary")
+    .rpc("tracelog_backup_summary")
 
   if(error){
 
