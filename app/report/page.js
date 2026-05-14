07f3681d899196fa60ed4ffe580ef276ffcdd55b
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-14 14:56:14 +0700

    Refactor PDF viewer to use SpecialZoomLevel

--- app/report/page.js
+++ app/report/page.js
@@ -3,6 +3,7 @@
 import {
   Viewer,
   Worker,
+  SpecialZoomLevel,
 } from "@react-pdf-viewer/core";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -15,21 +16,18 @@ export default function Page() {
         inset: 0,
         width: "100vw",
         height: "100vh",
-        overflow: "hidden",
         background: "#fff",
       }}
     >
       <Worker
         workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js"
       >
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <Viewer fileUrl="/api/report/pdf" />
-        </div>
+        <Viewer
+          fileUrl="/api/report/pdf"
+          defaultScale={
+            SpecialZoomLevel.PageWidth
+          }
+        />
       </Worker>
     </div>
   );
