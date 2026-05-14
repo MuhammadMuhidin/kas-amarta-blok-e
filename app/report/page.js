47e357706157fb85350f960f99a9df54ece5352c
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-14 14:14:42 +0700

    Refactor Page component to include global styles
    Added global styles for full viewport height and width.

--- app/report/page.js
+++ app/report/page.js
@@ -3,20 +3,47 @@
 import {
   Viewer,
   Worker,
+  SpecialZoomLevel,
 } from "@react-pdf-viewer/core";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export default function Page() {
   return (
-    <div style={{ height: "100vh" }}>
-      <Worker
-        workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js"
-      >
-        <Viewer
-          fileUrl="/api/report/pdf"
-        />
-      </Worker>
-    </div>
+    <>
+      <style jsx global>{`
+        html,
+        body,
+        #__next {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+        }
+
+        .rpv-core__viewer {
+          width: 100vw !important;
+          height: 100vh !important;
+        }
+
+        .rpv-core__page-layer {
+          margin: 0 auto !important;
+        }
+      `}</style>
+
+      <div className="fixed inset-0 bg-gray-200">
+        <Worker
+          workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js"
+        >
+          <Viewer
+            fileUrl="/api/report/pdf"
+            defaultScale={
+              SpecialZoomLevel.PageWidth
+            }
+          />
+        </Worker>
+      </div>
+    </>
   );
 }
