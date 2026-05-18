99e313256250cdb1a884a3a2bb82e157aa102651
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-18 00:25:02 +0000

    perPagePay

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function CashflowPage() {
   const [modalType, setModalType] = useState("last");
 
   const FETCH_URL = "/api/sheets/summary";
-  const perPagePay = 16;
+  const perPagePay = 20;
   const perPageInsight = 6;
   const chunk = 20;
 
