6e9ee9ba71c80fbf35d761bccbc577d1bc8540b2
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-21 10:41:47 +0700

    Add admin tab constants

---
+++ constants/adminTabs.js
@@ -0,0 +1,10 @@
+export const ADMIN_TABS = {
+  PERSONAL: "personal",
+  PAYMENT: "payment",
+  DEPOSIT: "deposit",
+  CASHFLOW: "cashflow",
+  SUMMARY: "summary",
+  MONITORING: "monitoring",
+  ACTIVITY: "activity",
+  SETTINGS: "settings",
+};
