f84552ed46d9160f07407d787300027947d6e60b
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-24 00:21:34 +0700

    Inject admin build info badge into layout

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -2,12 +2,14 @@ import "./theme.css";
 import "./sidebar.css";
 import AdminMobileDrawer from "@/components/admin/AdminMobileDrawer";
 import AdminThemeBoot from "@/components/admin/AdminThemeBoot";
+import AdminBuildInfoBadge from "@/components/admin/AdminBuildInfoBadge";
 
 export default function AdminLayout({ children }) {
   return (
     <>
       <AdminThemeBoot />
       <AdminMobileDrawer />
+      <AdminBuildInfoBadge />
       {children}
     </>
   );
