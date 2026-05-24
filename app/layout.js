3e4bc8e15104314c057582fb9f606fae865557f4
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-24 09:28:42 +0700

    Mount public theme picker globally

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,12 @@
+import PublicThemePicker from "@/components/public/PublicThemePicker";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body style={{ margin: 0 }}>
+        <PublicThemePicker />
         {children}
       </body>
     </html>
-  )
+  );
 }
