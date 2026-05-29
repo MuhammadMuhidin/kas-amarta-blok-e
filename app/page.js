e5997b825e647df90c2e020b240113b8304941bc
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-29 18:18:25 +0700

    Remove timeline DOM enhancer from resident page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
 import TimelineClient from "@/components/timeline/TimelineClient";
-import TimelineGalleryEnhancer from "@/components/timeline/TimelineGalleryEnhancer";
 import "@/app/page.css";
 import "@/app/public-theme.css";
 import "@/app/timeline.css";
 import "@/app/timeline-overrides.css";
 
 export default function ResidentTimelinePage() {
-  return (
-    <>
-      <TimelineClient />
-      <TimelineGalleryEnhancer />
-    </>
-  );
+  return <TimelineClient />;
 }
