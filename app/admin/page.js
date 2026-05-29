f0d2f691695626e4e5049c2f6a5be74f5170cb72
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-29 14:24:00 +0700

    Import admin timeline styles

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,6 +2,7 @@
 
 import AdminPageClient from "@/components/admin/AdminPageClient";
 import "./page.css";
+import "./timeline.css";
 
 export default function AdminPage() {
   return <AdminPageClient />;
