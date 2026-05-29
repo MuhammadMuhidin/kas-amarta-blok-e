bb94c0312ba8708fb3df6e2a4f84d1bec76825db
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-29 14:35:18 +0700

    Apply shared public theme styles to kas page

--- app/kas/page.js
+++ app/kas/page.js
@@ -2,6 +2,7 @@
 
 import PublicPageClient from "@/components/public/PublicPageClient";
 import "@/app/page.css";
+import "@/app/public-theme.css";
 
 export default function PublicKasPage() {
   return <PublicPageClient />;
