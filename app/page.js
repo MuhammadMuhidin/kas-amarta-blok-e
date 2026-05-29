41978381203c7d2638b46c4180890b1455fd04a6
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-29 18:01:34 +0700

    Mount timeline gallery enhancer

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import TimelineClient from "@/components/timeline/TimelineClient";
+import TimelineGalleryEnhancer from "@/components/timeline/TimelineGalleryEnhancer";
 import "@/app/page.css";
 import "@/app/public-theme.css";
 import "@/app/timeline.css";
 
 export default function ResidentTimelinePage() {
-  return <TimelineClient />;
+  return (
+    <>
+      <TimelineClient />
+      <TimelineGalleryEnhancer />
+    </>
+  );
 }
