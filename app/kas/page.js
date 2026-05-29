ff12c0eb8b45338a35bde552bf25095b1655cb27
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-05-29 13:05:05 +0700

    Add public kas route

---
+++ app/kas/page.js
@@ -0,0 +1,8 @@
+"use client";
+
+import PublicPageClient from "@/components/public/PublicPageClient";
+import "@/app/page.css";
+
+export default function PublicKasPage() {
+  return <PublicPageClient />;
+}
