76b6807f62fbff9a2e7f56cccd078d523b814e95
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-06 23:40:20 +0700

    Import admin interactions styles

--- app/admin/page.js
+++ app/admin/page.js
@@ -3,6 +3,7 @@
 import AdminPageClient from "@/components/admin/AdminPageClient";
 import "./page.css";
 import "./timeline.css";
+import "./interactions.css";
 
 export default function AdminPage() {
   return <AdminPageClient />;
