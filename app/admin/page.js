d3eb655042f6546df80f5cd4892dd4f53ac01dc2
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-07 14:25:03 +0700

    Remove admin mobile menu css import

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,7 +4,6 @@ import AdminPageClient from "@/components/admin/AdminPageClient";
 import "./page.css";
 import "./timeline.css";
 import "./interactions.css";
-import "./mobile-menu.css";
 
 export default function AdminPage() {
   return <AdminPageClient />;
