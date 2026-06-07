1dbaa0d46f272aaca3c2b89b386016d96746884c
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-07 13:46:04 +0700

    Import admin mobile menu styles

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,6 +4,7 @@ import AdminPageClient from "@/components/admin/AdminPageClient";
 import "./page.css";
 import "./timeline.css";
 import "./interactions.css";
+import "./mobile-menu.css";
 
 export default function AdminPage() {
   return <AdminPageClient />;
