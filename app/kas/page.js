4a272b12bfbc9b0f9340e4e73855c8bd7aa5b371
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-13 15:57:46 +0700

    fix: preload public theme state on kas

--- app/kas/page.js
+++ app/kas/page.js
@@ -1,6 +1,7 @@
 import PublicPageClient from "@/components/public/PublicPageClient";
 import "@/app/page.css";
 import "@/app/public-theme.css";
+import "@/app/public-theme-state.css";
 import "@/components/public/PublicThemePicker.css";
 import "./kas-page.css";
 
