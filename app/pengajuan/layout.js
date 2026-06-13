2e482e77a81b42ff52df37ac08dd567b9e62f2d0
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-13 18:16:30 +0700

    feat: enable initial pengajuan loading skeleton

--- app/pengajuan/layout.js
+++ app/pengajuan/layout.js
@@ -2,7 +2,14 @@ import "@/app/public-theme-state.css";
 import "@/components/public/PublicThemePicker.css";
 import "@/components/public/PublicBottomNav.css";
 import "./pengajuan.css";
+import "./master-loading.css";
+import PengajuanMasterLoadingGuard from "./PengajuanMasterLoadingGuard";
 
 export default function PengajuanLayout({ children }) {
-  return children;
+  return (
+    <>
+      <PengajuanMasterLoadingGuard />
+      {children}
+    </>
+  );
 }
