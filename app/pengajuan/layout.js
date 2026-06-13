4ab9f7e901eb86045f2b2e08a2b33f837cd6866a
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-13 15:53:27 +0700

    fix: load pengajuan critical styles in route layout

---
+++ app/pengajuan/layout.js
@@ -0,0 +1,7 @@
+import "@/components/public/PublicThemePicker.css";
+import "@/components/public/PublicBottomNav.css";
+import "./pengajuan.css";
+
+export default function PengajuanLayout({ children }) {
+  return children;
+}
