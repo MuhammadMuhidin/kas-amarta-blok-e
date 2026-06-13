9252a6c0b329b61d4576006f48b638885ec02709
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-14 06:51:38 +0700

    refactor(kas): remove hero from loading state

--- components/public/KasLoadingView.js
+++ components/public/KasLoadingView.js
@@ -1,18 +1,8 @@
-import PublicHero from "@/components/public/PublicHero";
 import PublicRouteSkeleton from "@/components/public/PublicRouteSkeleton";
 
 export default function KasLoadingView() {
   return (
-    <div className="page-wrap">
-      <PublicHero
-        description={(
-          <>
-            Pusat transparansi iuran, pengeluaran,
-            <br />
-            dan laporan kas warga.
-          </>
-        )}
-      />
+    <div className="page-wrap public-kas-page">
       <PublicRouteSkeleton variant="kas" includeHero={false} />
     </div>
   );
