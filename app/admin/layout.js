43f46b051506a5c40ffff21812ec5b939b982e74
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-14 15:11:54 +0700

    fix: mount deduplicated approval lifecycle controller

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -7,7 +7,7 @@ import "./master-management-english.css";
 import AdminMobileDrawer from "@/components/admin/AdminMobileDrawer";
 import AdminThemeBoot from "@/components/admin/AdminThemeBoot";
 import AdminTopActions from "@/components/admin/AdminTopActions";
-import ApprovalMasterLifecycleController from "@/components/admin/ApprovalMasterLifecycleController";
+import ApprovalMasterLifecycleControllerV2 from "@/components/admin/ApprovalMasterLifecycleControllerV2";
 
 export default function AdminLayout({ children }) {
   return (
@@ -15,7 +15,7 @@ export default function AdminLayout({ children }) {
       <AdminThemeBoot />
       <AdminMobileDrawer />
       <AdminTopActions />
-      <ApprovalMasterLifecycleController />
+      <ApprovalMasterLifecycleControllerV2 />
       {children}
     </>
   );
