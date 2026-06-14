6edcf114b213a978aa8a4f2c4ad29beedea1ee14
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-14 08:44:15 +0700

    feat: enable read-only master preview

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -3,10 +3,12 @@ import "./ledger-theme.css";
 import "./sidebar.css";
 import "./master-version-diff.css";
 import "./master-management-actions.css";
+import "./master-readonly-preview.css";
 import AdminMobileDrawer from "@/components/admin/AdminMobileDrawer";
 import AdminThemeBoot from "@/components/admin/AdminThemeBoot";
 import AdminTopActions from "@/components/admin/AdminTopActions";
 import MasterManagementActionFix from "@/components/admin/MasterManagementActionFix";
+import MasterManagementReadOnlyPreview from "@/components/admin/MasterManagementReadOnlyPreview";
 import MasterVersionHistoryDiff from "@/components/admin/MasterVersionHistoryDiff";
 
 export default function AdminLayout({ children }) {
@@ -16,6 +18,7 @@ export default function AdminLayout({ children }) {
       <AdminMobileDrawer />
       <AdminTopActions />
       <MasterManagementActionFix />
+      <MasterManagementReadOnlyPreview />
       <MasterVersionHistoryDiff />
       {children}
     </>
