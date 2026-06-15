2ae56b1fac5413cc245342edaddc4ef9494612be
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-06-16 01:40:07 +0700

    Load admin layout fixes

--- app/admin/page.js
+++ app/admin/page.js
@@ -5,6 +5,7 @@ import "./page.css";
 import "./timeline.css";
 import "./timeline-photo-management.css";
 import "./interactions.css";
+import "./layout-fixes.css";
 
 export default function AdminPage() {
   return <AdminPageClient />;
